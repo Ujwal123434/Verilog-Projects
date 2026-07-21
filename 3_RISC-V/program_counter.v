c539cbc3b65fb65b41230787cc8c88d533aba666
Ujwal123434 <ujwalkumar.l2005@gmail.com>
2026-07-21 15:04:01 +0530

    Program counter file

---
+++ 3_RISC-V/program_counter.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module program_counter(
+        input clk,rst,
+        input [31:0] pc_in,
+        output reg [31:0] pc_out 
+    );
+    
+    always@(posedge clk) begin
+      if(rst)
+          pc_out <= 32'b00;
+      else
+          pc_out <= pc_in;
+     end
+endmodule
